c55c27e4c4103b5af32fce750ddbba4b30f691f6
Hayden White <57922826+Skyibis@users.noreply.github.com>
2023-12-02 12:01:15 -0500

    Small update

--- src/pipeline/detection.py
+++ src/pipeline/detection.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
+import logging
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('YOLO_Detection')
 
 class Model(object):
 
     def __init__(self, model_path: str):
-        self.model = YOLO(model_path)
+        self.model = YOLO(model_path) 
     
     def predict(self, frame):
-        return self.model(frame)
+        logger.debug('Running YOLOv8 inference on the frame')
+        return self.model(frame, verbose=False)
         
