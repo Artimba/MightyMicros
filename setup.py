87effce0c23b04966dd759095ce9b943187ed03b
Hayden White <57922826+Skyibis@users.noreply.github.com>
2023-12-10 16:54:09 -0500

    mmcv installation attempt

--- setup.py
+++ setup.py
@@ -1,34 +1,40 @@
-"""
-This initializes our code allowing us to import across submodules. 
-If you are getting import errors, run pip install -e . while your inside the /MightyMicros/ (project root) directory.
-"""
 from setuptools import setup, find_packages
-import os
+from setuptools.command.install import install
 import subprocess
 import sys
+
+class CustomInstall(install):
+    def run(self):
+        # Install mmcv-full, mmdet, and mmrotate using mim
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'openmim'])
+        subprocess.check_call(['mim', 'install', 'mmcv-full==1.7.1'])
+        subprocess.check_call(['mim', 'install', 'mmdet==2.28.2'])
+        subprocess.check_call(['mim', 'install', 'mmrotate==0.3.4'])
+
+        # Proceed with the standard installation
+        install.run(self)
 
 def is_cuda_available():
     try:
         subprocess.check_output(['nvcc', '--version'])
+        return True
     except subprocess.CalledProcessError:
         return False
     except FileNotFoundError:
         return False
 
 requirements = [
-            'pykalman',
-            'pyqt5',
-            'requests==2.28.2',
-            'jupyterlab-server==2.19.0',
-            'notebook==6.4.13',
-            'tensorboard',
-            'opencv-python-headless',
-            'opencv-contrib-python',
-            'tqdm'
-            'mmcv-full==1.7.1',
-            'mmdet==2.28.2',
-            'mmrotate==0.3.4'
-        ]
+    'pykalman',
+    'pyqt5',
+    'requests==2.28.2',
+    'jupyterlab-server==2.19.0',
+    'notebook==6.4.13',
+    'tensorboard',
+    'opencv-python-headless',
+    'opencv-contrib-python',
+    'tqdm',
+    'openmim'
+]
 
 if __name__ == '__main__':
     if is_cuda_available():
@@ -37,15 +43,18 @@ if __name__ == '__main__':
     else:
         requirements.append('torch==2.1.1+cpu')
         requirements.append('torchvision==0.16.1+cpu')
-    # install_packages()
+
     setup(
         name='mighty-micros', 
         version='0.4', 
         packages=find_packages(),
         entry_points={
-                'console_scripts': [
-                    'mighty-micros = src.entry:run'
-                ]
-            },
+            'console_scripts': [
+                'mighty-micros = src.entry:run'
+            ]
+        },
         install_requires=requirements,
-        )
+        cmdclass={
+            'install': CustomInstall,
+        }
+    )
