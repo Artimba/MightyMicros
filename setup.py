fbf06a498d8abccf365dd76a0287b840afb76ef7
Hayden White <57922826+Skyibis@users.noreply.github.com>
2023-12-10 16:47:02 -0500

    First auto install attempt

--- setup.py
+++ setup.py
@@ -7,20 +7,15 @@ import os
 import subprocess
 import sys
 
+def is_cuda_available():
+    try:
+        subprocess.check_output(['nvcc', '--version'])
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
 
-if __name__ == '__main__':
-    
-    # install_packages()
-    setup(
-        name='mighty-micros', 
-        version='0.3', 
-        packages=find_packages(),
-        entry_points={
-                'console_scripts': [
-                    'mighty-micros = src.entry:run'
-                ]
-            },
-        install_requires=[
+requirements = [
             'pykalman',
             'pyqt5',
             'requests==2.28.2',
@@ -34,4 +29,23 @@ if __name__ == '__main__':
             'mmdet==2.28.2',
             'mmrotate==0.3.4'
         ]
+
+if __name__ == '__main__':
+    if is_cuda_available():
+        requirements.append('torch==2.1.1+cu121')
+        requirements.append('torchvision==0.16.1+cu121')
+    else:
+        requirements.append('torch==2.1.1+cpu')
+        requirements.append('torchvision==0.16.1+cpu')
+    # install_packages()
+    setup(
+        name='mighty-micros', 
+        version='0.4', 
+        packages=find_packages(),
+        entry_points={
+                'console_scripts': [
+                    'mighty-micros = src.entry:run'
+                ]
+            },
+        install_requires=requirements,
         )
